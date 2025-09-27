a6bcc76a11a0491d9a42f4ea8406cc5316f3c68d
Trey <phlasc@gmail.com>
2025-09-27 14:22:55 -0400

    Implement code changes to enhance functionality and improve performance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // https://nuxt.com/modules
-  modules: ['@nuxthub/core', '@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxthub/core', '@nuxt/ui', '@nuxt/eslint', 'nuxt-resend'],
   devtools: { enabled: true },
 
 css: ["~/assets/css/main.css"],
